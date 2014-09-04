34e7850e7fea2c6b2956a1903248762727e4c66a
PNAryanUA <nazariy.papizh@gmail.com>
2014-09-04 17:58:52 +0300

    pre release 4.01

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
     doc.to_html.html_safe
   end
 
+  def self.embedded_svg_js filename, options={}
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+  end
+
 end
